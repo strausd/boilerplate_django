399be980ed3251d39b284a70e5be4ebc4d408b81
Danny Straus <dstraus@insite.net>
2018-01-10 17:51:35 -0600

    Working on collectstatic on heroku

--- boilerplate_django/settings/heroku.py
+++ boilerplate_django/settings/heroku.py
@@ -5,3 +5,9 @@ import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
 ALLOWED_HOSTS = ['strausd-boilerplate-django.herokuapp.com']
+
+MIDDLEWARE += [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
